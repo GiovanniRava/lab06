32c7b5b899b7dd913d2dbeac8dc954c0fed54661
Giovanni Maria Rava <giovanni.maria.rava@gmail.com>
2024-12-04 18:46:49 +0100

    added UseLIstAndMap modified

--- 62-use-lists-and-maps/src/main/java/it/unibo/collections/UseListsAndMaps.java
+++ 62-use-lists-and-maps/src/main/java/it/unibo/collections/UseListsAndMaps.java
@@ -12,6 +12,9 @@ import java.util.Map;
  */
 public final class UseListsAndMaps {
 
+    private static final int END = 2000;
+    private static final int START = 1000;
+
     private UseListsAndMaps() {
     }
 
@@ -24,64 +27,73 @@ public final class UseListsAndMaps {
          * 1) Create a new ArrayList<Integer>, and populate it with the numbers
          * from 1000 (included) to 2000 (excluded).
          */
-        ArrayList <Integer> arrist = new ArrayList<>();
-        int j=0;
-        for(int i=0;j<=2000;i++){
-            arrist[i]= j;
-            j++;
+        final List <Integer> arrist = new ArrayList<>();
+        
+        for(int i=START;i<=END;i++){
+            arrist.add(i);
         }
         /*
          * 2) Create a new LinkedList<Integer> and, in a single line of code
          * without using any looping construct (for, while), populate it with
          * the same contents of the list of point 1.
          */
-        LinkedList<Integer> linkist = new LinkedList<>();
-        linkist.addAll(Collection<? extends Integer>);
-        /*populateList(linkist, 2000);
-        private static void populateList(LinkedList<Integer> list, int n) {
-            if (n == 1000) return; // Condizione di uscita
-            populateList(list, n - 1); // Chiamata ricorsiva per l'elemento precedente
-            list.add(n); // Aggiunge l'elemento corrente dopo la chiamata ricorsiva
-        }*/
+        List<Integer> linkist = new LinkedList<>(arrist);
+        log(linkist);
+        
         /*
-         * 3) Using "set" and "get" and "size" methods, swap the first and last
-         * element of the first list. You can not use any "magic number".
-         * (Suggestion: use a temporary variable)
-         */
+            * 3) Using "set" and "get" and "size" methods, swap the first and last
+            * element of the first list. You can not use any "magic number".
+            * (Suggestion: use a temporary variable)
+            */
+        int temp= arrist.getLast();
+        arrist.set(arrist.size()-1, arrist.getFirst());
+        arrist.set(0, temp);
         /*
-         * 4) Using a single for-each, print the contents of the arraylist.
-         */
+            * 4) Using a single for-each, print the contents of the arraylist.
+            */
+        final var builder = new StringBuilder();
+        for (final int i : arrist) {
+            builder.append(i).append(", ");
+        }
+        if(!builder.isEmpty()){
+            builder.delete(builder.length() - 2, builder.length());
+        }
+        log(builder);
         /*
-         * 5) Measure the performance of inserting new elements in the head of
-         * the collection: measure the time required to add 100.000 elements as
-         * first element of the collection for both ArrayList and LinkedList,
-         * using the previous lists. In order to measure times, use as example
-         * TestPerformance.java.
-         */
+            * 5) Measure the performance of inserting new elements in the head of
+            * the collection: measure the time required to add 100.000 elements as
+            * first element of the collection for both ArrayList and LinkedList,
+            * using the previous lists. In order to measure times, use as example
+            * TestPerformance.java.
+            */
         /*
-         * 6) Measure the performance of reading 1000 times an element whose
-         * position is in the middle of the collection for both ArrayList and
-         * LinkedList, using the collections of point 5. In order to measure
-         * times, use as example TestPerformance.java.
-         */
+            * 6) Measure the performance of reading 1000 times an element whose
+            * position is in the middle of the collection for both ArrayList and
+            * LinkedList, using the collections of point 5. In order to measure
+            * times, use as example TestPerformance.java.
+            */
         /*
-         * 7) Build a new Map that associates to each continent's name its
-         * population:
-         *
-         * Africa -> 1,110,635,000
-         *
-         * Americas -> 972,005,000
-         *
-         * Antarctica -> 0
-         *
-         * Asia -> 4,298,723,000
-         *
-         * Europe -> 742,452,000
-         *
-         * Oceania -> 38,304,000
-         */
-        /*
-         * 8) Compute the population of the world
-         */
-    }
+            * 7) Build a new Map that associates to each continent's name its
+            * population:
+            *
+            * Africa -> 1,110,635,000
+            *
+            * Americas -> 972,005,000
+            *
+            * Antarctica -> 0
+            *
+            * Asia -> 4,298,723,000
+            *
+            * Europe -> 742,452,000
+            *
+            * Oceania -> 38,304,000
+            */
+            /*
+                * 8) Compute the population of the world
+                */
+        }
+        private static void log(final Object message) {
+            System.out.println(message); // NOPMD
+        }
+           
 }
